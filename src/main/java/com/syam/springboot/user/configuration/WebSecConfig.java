dfbe1e183d17340beada6580c22d945d82d1ff5a
syamkumarm1983 <155859978+syamkumarm1983@users.noreply.github.com>
2024-02-04 16:57:15 -0600

    Logenable (#3)
    * adding log
    
    * updated header validation
    
    * getting all request info from filers

--- src/main/java/com/syam/springboot/user/configuration/WebSecConfig.java
+++ src/main/java/com/syam/springboot/user/configuration/WebSecConfig.java
@@ -1,17 +1,21 @@
 package com.syam.springboot.user.configuration;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Slf4j
 @Configuration
 public class WebSecConfig implements WebMvcConfigurer{
 
     @Bean
     public WebMvcConfigurer corsConfigurer()
     {
+        log.info("WebMvcConfigurer");
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
@@ -19,6 +23,17 @@ public class WebSecConfig implements WebMvcConfigurer{
                         .allowedOrigins("http://localhost:8080","http://localhost:3000","http://localhost:3001");
             }
         };
+    }
+
+    @Bean
+    public CommonsRequestLoggingFilter requestLoggingFilter() {
+        CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
+        loggingFilter.setIncludeClientInfo(true);
+        loggingFilter.setIncludeQueryString(true);
+        loggingFilter.setIncludePayload(true);
+        loggingFilter.setIncludeHeaders(true);
+        loggingFilter.setMaxPayloadLength(64000);
+        return loggingFilter;
     }
 
     @Override
